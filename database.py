2bb3c43a38c457b64cd15de14fc18563a80421a7
Oleg Arslanov <olegarslanov@email.com>
2023-05-20 08:46:08 +0300

    dadejau database.py

---
+++ database.py
@@ -0,0 +1,7 @@
+from pymongo import MongoClient
+from pymongo.database import Database
+
+
+def get_database() -> Database:
+    client = MongoClient("mongodb://localhost:27017/")
+    return client["task_manager"]
