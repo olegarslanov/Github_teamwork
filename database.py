7ee0a0ec50898f0a732b9163f124bb4c42dbc1b6
Remigijus-Samonskis <124807089+Remigijus-Samonskis@users.noreply.github.com>
2023-05-22 14:17:06 +0300

    Update database.py

--- database.py
+++ database.py
@@ -5,3 +5,30 @@ from pymongo.database import Database
 def get_database() -> Database:
     client = MongoClient("mongodb://localhost:27017/")
     return client["task_manager"]
+
+from typing import List, Dict, Any
+from database import get_database
+from pymongo.collection import Collection
+
+def add_task(task: Dict[str, Any]) -> str:
+    db = get_database()
+    collection = db['tasks']
+    result = collection.insert_one(task)
+    return str(result.inserted_id)
+
+def view_all_tasks() -> List[Dict[str, Any]]:
+    db = get_database()
+    collection = db['tasks']
+    return list(collection.find())
+
+def update_task_status(task_id: str, status: str) -> int:
+    db = get_database()
+    collection = db['tasks']
+    result = collection.update_one({'_id': task_id}, {'$set': {'status': status}})
+    return result.modified_count
+
+def delete_task(task_id: str) -> int:
+    db = get_database()
+    collection = db['tasks']
+    result = collection.delete_one({'_id': task_id})
+    return result.deleted_count
